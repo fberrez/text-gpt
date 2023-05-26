8aa5b0c8442e515d5d51e56c65a646a297862e84
fberrez <florent.berrez@gmail.com>
2023-05-26 15:41:08 +0200

    feat(mongo): add connection handling

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const config = require('./config');
 const server = require('./lib/server');
+const mongoose = require('./lib/backend/mongo');
 
-server.listen({port: config.PORT, host: config.HOST}, (err, address) => {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-	}
+async function main() {
+	await mongoose.connect(config.MONGO_URL);
+	server.listen({port: config.PORT, host: config.HOST}, (err, address) => {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
 
-	console.log(`Server listening at ${address}`);
-});
+		console.log(`Server listening at ${address}`);
+	});
+}
+
+async function shutdown() {
+	console.log('SIGTERM signal received: closing HTTP server and mongoDB connection');
+	await mongoose.disconnect();
+	await server.close();
+	console.log('HTTP server closed');
+	process.exit(0);
+}
+
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
+
+main();
 
